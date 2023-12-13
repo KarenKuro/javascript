57b054f4f7b59679c46ba48704bdefb00b39beb9
KarenKuro <kkarenk888@gmail.com>
2023-12-13 16:25:24 +0400

    millioner and capitals quiz

---
+++ millioner.js
@@ -0,0 +1,114 @@
+let arrQuestions = [
+    "What is the capital of Armenia?Yerevan,Moscow,Washington,Tbilisi",
+    "What is the capital of Russia?Moscow,Yerevan,Washington,Tbilisi",
+    "What is the capital of USA?Washington,Yerevan,Moscow,Tbilisi",
+    "What is the capital of Georgia?Tbilisi,Yerevan,Moscow,Washington",
+    "What is the capital of France?Paris,London,Berlin,Madrid",
+    "What is the capital of GB?London,Berlin,Madrid,Paris",
+    "What is the capital of Germany?Berlin,London,Madrid,Paris",
+    "What is the capital of Spain?Madrid,Paris,London,Berlin",
+    "What is the capital of Italy?Rome,Tokio,Astana,Beijing",
+    "What is the capital of Japan?Tokio,Rome,Astana,Beijing",
+    "What is the capital of Kazakhstan?Astana,Tokio,Rome,Beijing",
+    "What is the capital of China?Beijing,Astana,Tokio,Rome",
+    "What is the capital of Austria?Vien,Cairo,Warshaw,Athens",
+    "What is the capital of Egypt?Cairo,Warshaw,Athens,Vien",
+    "What is the capital of Poland?Warshaw,Cairo,Athens,Vien",
+    "What is the capital of Greece?Athens,Warshaw,Cairo,Vien",
+    "What is the capital of Norway?Oslo,Vilnus,Helsinki,Stockholm",
+    "What is the capital of Finland?Helsinki,Oslo,Vilnus,Stockholm",
+    "What is the capital of Sweden?Stockholm,Helsinki,Oslo,Vilnus",
+    "What is the capital of Lithuania?Vilnus,Stockholm,Helsinki,Oslo",
+    "What is the capital of Estonia?Talin,Riga,Kiev,Minsk",
+    "What is the capital of Latvia?Riga,Talin,Kiev,Minsk",
+    "What is the capital of Ukraine?Kiev,Riga,Talin,Minsk",
+    "What is the capital of Belarus?Minsk,Kiev,Riga,Talin",
+]
+
+// Выбираем рандомные 10 вопросов
+
+function chouseRandomQwest (arr, quantityOfQwest) {
+    let finalyArr = [];
+    let arrRandomIndex = [];
+
+    for (let i = 0; i < quantityOfQwest; i++ ){
+        let randomIndex = Math.floor(Math.random() * arr.length);
+        if (arrRandomIndex.includes(randomIndex)){
+            i--;
+        }
+        else {
+            arrRandomIndex.push(randomIndex)
+        }
+    }
+
+    for (let i = 0; i < arrRandomIndex.length; i++){
+        finalyArr.push(arr[arrRandomIndex[i]]);
+    }
+
+    return finalyArr
+}
+
+
+// Разделяем вопрос и варианты ответов
+
+function sepQwestAndAnswers () {
+    let myArr = chouseRandomQwest(arrQuestions, 10);
+    let finalyObj = {};
+
+    for (let i = 0; i < myArr.length; i++){
+        let splitedQwest = myArr[i].split('?');
+        finalyObj[splitedQwest[0] + '?'] = splitedQwest[1].split(',');
+    }
+return finalyObj
+}
+
+
+// Сам процесс игры
+
+function playTheGame () {
+    let qwestAndAnsw = sepQwestAndAnswers();
+    let rightAnswers = 0;
+
+    for (let key in qwestAndAnsw) {
+        correctAnswer = qwestAndAnsw[key][0];
+        let variantOfAnswer = qwestAndAnsw[key];
+        let arrRandomIndex = [];
+        let arrRandomAnswers = [];
+        
+        // Создаем массив с рандомными числами, которые станут индексами массива с вариантами ответов 
+        
+        for (let i = 0; i < variantOfAnswer.length; i++ ){             
+            let randomIndex = Math.floor( Math.random() * variantOfAnswer.length);
+            
+            if (arrRandomIndex.includes(randomIndex)){
+                i--;
+            }
+            else {
+                arrRandomIndex.push(randomIndex)
+            }
+        }
+        
+        // В пустой массив добавляем варианты ответов, выбирая их по рандомному индексу (arrRandomIndex)
+        
+         for (let j = 0; j < arrRandomIndex.length; j++ ){
+            arrRandomAnswers.push(variantOfAnswer[arrRandomIndex[j]]);
+        }
+
+        // Задаем вопрос, сравниваем ответ с правильным ответом и считаем их кол-во
+
+        answer = prompt(`${key} ${arrRandomAnswers.join(", ")}`);
+        if (answer == null) {
+            break;
+        }
+        else{
+            if (answer.toLowerCase() == correctAnswer.toLowerCase() ) {
+                rightAnswers++;
+            }
+        } 
+    }
+
+    alert(`У вас ${rightAnswers} правильных ответов!`)
+}
+
+
+playTheGame()
---
+++ theCapitalsQuiz.js
@@ -0,0 +1,124 @@
+let countryCapitalObj = {
+    Armenia : "Yerevan",
+    Russia : "Moscow",
+    USA : "Washington",
+    Georgia : "Tbilisi",
+    France : "Paris",
+    GB : "London",
+    Germany : "Berlin",
+    Spain : "Madrid",
+    Italy : "Rome",
+    Japan : "Tokio",
+    Kazakhstan : "Astana",
+    China : "Beijing",
+    Austria : "Vien",
+    Egypt : "Cairo",
+    Poland : "Warshaw",
+    Greece : "Athens",
+    Norway : "Oslo",
+    Finland : "Helsinki",
+    Sweden : "Stockholm",
+    Lithuania : "Vilnus",
+    Estonia : "Talin",
+    Latvia : "Riga",
+    Ukraine : "Kiev",
+    Belarus : "Minsk",
+    Chile : "Santiago",
+} 
+
+
+// Квиз до одной ошибки
+
+function playUntilFirstMistake(obj) {
+    let correctAnswers = 0;
+
+    alert("Let's play!!!")
+
+    for (let key in obj) {
+        let answer = prompt("What is the capital of " + key + "?" );
+        
+        if (answer == null) {
+            alert("Game over, chuvak!!!");
+            break;
+        }
+        
+        if (answer.toLowerCase() == obj[key].toLowerCase()){
+            correctAnswers++;
+        }
+        else {
+            alert("Your answer was wrong. Game over");
+            break;
+        }
+    }
+}
+
+
+// Квиз с возможностью ошибиться 3 раза
+
+function canBeWrong3Times(obj) {
+    let correctAnswers = 0;
+    let wrongAnswers = 0;
+
+    alert("Let's play!!! You can be wrong 3 times")
+
+    for (let key in obj) {
+        let answer = prompt("What is the capital of " + key + "?" );
+        
+        if (answer == null) {
+            alert("Game over, chuvak!!!");
+            break;
+        }
+        
+        if (answer.toLowerCase() == obj[key].toLowerCase()){
+            correctAnswers++;
+        }
+        else {
+            wrongAnswers++;
+            if (wrongAnswers > 3){
+                alert ("Game over, chuvak!!!")
+                break;       
+            }
+            alert(`You answered wrong ${wrongAnswers} times`);
+        }
+    }
+}
+
+
+// Квиз с ограничением по времени
+function withTimeLimit(obj){
+
+    let correctAnswers = 0;
+    let executionTime = 0;
+
+    alert("Let's play!!! You have 120 second !")
+
+    for (let key in obj) {
+        let startTime = performance.now();
+
+        let answer = prompt("What is the capital of " + key + "?" );
+        if (answer == null) {
+            alert("Game over, chuvak!!!");
+            break;
+        }
+        
+        if (answer.toLowerCase() == obj[key].toLowerCase()){
+            correctAnswers++;
+        }
+
+        let endTime = performance.now();
+        let executionTimeOfOneQwestion = (endTime-startTime)/1000;
+        executionTime += executionTimeOfOneQwestion;
+
+        if (executionTime > 120) {
+            alert("The time is over");
+            break;
+        }
+    }
+    alert(`TOTAL. Correct answers: ${correctAnswers} !!!`)
+}
+
+
+
+// playUntilFirstMistake(countryCapitalObj);
+// canBeWrong3Times(countryCapitalObj);
+// withTimeLimit(countryCapitalObj); 
